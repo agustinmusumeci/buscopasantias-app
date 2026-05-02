517a32bfca73f90a977e609468a221bc6174290e
agustinmusumeci <agu.musumeci@gmail.com>
2026-05-02 13:58:04 -0300

    feat: Index skeleton. Center about page content

--- src/components/termsAndConditions/collapsibleIndex.tsx
+++ src/components/termsAndConditions/collapsibleIndex.tsx
@@ -1,36 +1,35 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { TERMS_PAGES } from "@/constants/termsPages";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { useDesktopResolution } from "@/hooks/useResolution";
 import Index from ".";
 
-
-export default function CollapsibleIndex({ prefix, currentURI }: { prefix: string, currentURI: string }) {
-  const isDesktop = useDesktopResolution({ resolution: 1024 });
+export default function CollapsibleIndex({ prefix, currentURI }: { prefix: string; currentURI: string }) {
+  const isDesktop = useDesktopResolution({ resolution: 768 });
 
   return (
     <>
-      {
-        isDesktop
-          ?
-          <Index currentURI={currentURI} prefix={prefix} />
-          :
-          <Accordion type="single" collapsible defaultValue="index" >
-            <AccordionItem value="index">
-              <AccordionTrigger className="[&>*:first-child]:flex! button button-ghost bg-light-neutral/50 w-full! rounded-full px-5 py-3"
-              >Navegación<nav></nav></AccordionTrigger>
+      {isDesktop ? (
+        <Index
+          currentURI={currentURI}
+          prefix={prefix}
+        />
+      ) : (
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full"
+        >
+          <AccordionItem value="index">
+            <AccordionTrigger className="[&>*:first-child]:flex! button button-ghost bg-light-neutral/50 w-full! rounded-full px-5 py-3">Navegación</AccordionTrigger>
 
-              <AccordionContent className="mt-5">
-                <Index currentURI={currentURI} prefix={prefix} />
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion >
-      }
+            <AccordionContent className="mt-5">
+              <Index
+                currentURI={currentURI}
+                prefix={prefix}
+              />
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      )}
     </>
-
-  )
+  );
 }
